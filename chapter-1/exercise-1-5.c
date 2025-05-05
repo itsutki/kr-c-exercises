adcbd4adb1c5f2f60596c190eeb1a078fb8770db
itsutki <utkugaspak@gmail.com>
2025-05-05 14:20:24 +0200

    Basic for loop exercise with C

--- chapter-1/exercise-1-5.c
+++ chapter-1/exercise-1-5.c
@@ -5,8 +5,10 @@ int main(){
     //fahrenheit = celcius * 9/5 + 32
     double fahrenheit, celcius;
 
+    printf("Celcius-Fahrenheit\n");
     for (celcius = -10; celcius <= 100; celcius += 10){
         fahrenheit = celcius * 9.0/5 + 32;
-        printf("%6.1f %6.1f \n", fahrenheit,celcius);
+        
+        printf("%6.1f %6.1f \n", celcius,fahrenheit);
     }
 }
