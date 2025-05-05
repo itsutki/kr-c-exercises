597e2c2b154f5b8e54d793baa6ab743af1c045f8
itsutki <utkugaspak@gmail.com>
2025-05-05 14:16:33 +0200

    Basic for loop exercise with C

---
+++ chapter-1/exercise-1-5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// celcius to fahrenheit converter
+int main(){
+    //fahrenheit = celcius * 9/5 + 32
+    double fahrenheit, celcius;
+
+    for (celcius = -10; celcius <= 100; celcius += 10){
+        fahrenheit = celcius * 9/5 + 32;
+        printf("%6.1f %6.1f \n", fahrenheit,celcius);
+    }
+}
